6a6192047d885fc35f58ff89321b1c85726cc3d3
HuberyTest <frogshero@qq.com>
2019-12-30 19:29:28 +0800

    change layout

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -43,39 +43,39 @@ class WeatherForecast extends React.Component {
     render() {
         const ops = this.state.cities.map((item) => <option key={item.id} value={item.id}>{item.name}</option>);
         return (
-            <div>
+            <div className="Main">
                 <h2>Current weather in your city</h2>
 
                 <select onChange={(e) => this.forecastCity(Number(e.target.value))}>
                     {ops}
                 </select>
                 <div>&nbsp;</div>
-                <div>
-                    <table>
-                    <tbody>
-                    <tr>
-                        <td>City</td>
-                        <td>{this.state.selName}</td>
-                    </tr>
-                    <tr>
-                        <td>Updated time</td>
-                        <td>{this.state.updateTime}</td>
-                    </tr>
-                    <tr>
-                        <td>Weather</td>
-                        <td>{this.state.weather}</td>
-                    </tr>
-                    <tr>
-                        <td>Temperature</td>
-                        <td>{this.state.temperature} &#8451;</td>
-                    </tr>
-                    <tr>
-                        <td>Wind</td>
-                        <td>{this.state.windSpeed} m/s</td>
-                    </tr>
-                    </tbody>
-                    </table>
-                </div>
+
+                <table>
+                <tbody>
+                <tr>
+                    <td>City</td>
+                    <td>{this.state.selName}</td>
+                </tr>
+                <tr>
+                    <td>Updated time</td>
+                    <td>{this.state.updateTime}</td>
+                </tr>
+                <tr>
+                    <td>Weather</td>
+                    <td>{this.state.weather}</td>
+                </tr>
+                <tr>
+                    <td>Temperature</td>
+                    <td>{this.state.temperature} &#8451;</td>
+                </tr>
+                <tr>
+                    <td>Wind</td>
+                    <td>{this.state.windSpeed} m/s</td>
+                </tr>
+                </tbody>
+                </table>
+
             </div>
         );
     }
