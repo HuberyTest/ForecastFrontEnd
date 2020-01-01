9130435eef510aa816245c20924f165996195049
HuberyTest <frogshero@qq.com>
2020-01-01 17:25:14 +0800

    add cross-env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import fetchMock from 'fetch-mock';
+
+fetchMock.config.fallbackToNetwork = 'always';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
